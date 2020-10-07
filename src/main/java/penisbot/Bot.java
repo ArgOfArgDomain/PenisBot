eb1bf3d362d81f455fe328c93df9030c373e2b90
ArgOfArgDomain <8324748+ArgOfArgDomain@users.noreply.github.com>
2020-10-07 20:13:27 -0300

    Basic user repository.

--- src/main/java/penisbot/Bot.java
+++ src/main/java/penisbot/Bot.java
@@ -1,12 +1,12 @@
 package penisbot;
 
 import org.jibble.pircbot.IrcException;
-import org.jibble.pircbot.NickAlreadyInUseException;
 import org.jibble.pircbot.PircBot;
 import org.jibble.pircbot.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import penisbot.configuration.Configuration;
+import penisbot.state.UserRepository;
 
 import java.io.IOException;
 
@@ -15,6 +15,7 @@ public class Bot extends PircBot {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private final Configuration configuration;
+    private final UserRepository userRepository = new UserRepository();
 
     public Bot(Configuration configuration) {
         super();
@@ -58,6 +59,9 @@ public class Bot extends PircBot {
 
         logger.info("Disconnected");
 
+        // Clear entire user repository
+        userRepository.clearAll();
+
         // Attempt to reconnect forever
         // We are at this point blocking the main bot thread, but this should not matter.
         while (!isConnected()) {
@@ -79,11 +83,17 @@ public class Bot extends PircBot {
         super.onConnect();
 
         logger.info("Connected");
+
     }
 
     @Override
     protected void onUserList(String channel, User[] users) {
         super.onUserList(channel, users);
+
+        // Add to our user repository
+        for (User user : users) {
+            userRepository.upsertUser(user.getNick(), channel);
+        }
     }
 
     @Override
@@ -94,25 +104,58 @@ public class Bot extends PircBot {
     @Override
     protected void onJoin(String channel, String sender, String login, String hostname) {
         super.onJoin(channel, sender, login, hostname);
+
+        // Add to our user repository
+        userRepository.upsertUser(sender, channel);
     }
 
     @Override
     protected void onPart(String channel, String sender, String login, String hostname) {
         super.onPart(channel, sender, login, hostname);
+        logger.debug("User " + sender + " left channel " + channel);
+
+        // Generic "user (possibly us) left for reason" handling
+        handleUserLeavingChannel(channel, sender);
+
     }
 
     @Override
     protected void onNickChange(String oldNick, String login, String hostname, String newNick) {
         super.onNickChange(oldNick, login, hostname, newNick);
+        logger.debug("User " + oldNick + " changed name to " + newNick);
+
+        // Rename in our user repository
+        userRepository.renameUser(oldNick, newNick);
     }
 
     @Override
     protected void onKick(String channel, String kickerNick, String kickerLogin, String kickerHostname, String recipientNick, String reason) {
         super.onKick(channel, kickerNick, kickerLogin, kickerHostname, recipientNick, reason);
+        logger.debug("User " + recipientNick + " kicked from channel " + channel);
+
+        // Generic "user (possibly us) left for reason" handling
+        handleUserLeavingChannel(channel, recipientNick);
     }
 
     @Override
     protected void onQuit(String sourceNick, String sourceLogin, String sourceHostname, String reason) {
         super.onQuit(sourceNick, sourceLogin, sourceHostname, reason);
+        logger.debug("User " + sourceNick + " quit");
+
+        // Remove from our user repository entirely
+        userRepository.removeUser(sourceNick);
+
+    }
+
+    private void handleUserLeavingChannel(String channel, String nickname) {
+        // Was this us?
+        if (nickname == getName()) {
+            // Remove everyone from this channel, as we no longer know about what
+            // is going on in there.
+            userRepository.removeChannel(channel);
+        } else {
+            // Remove from channel in our user repository
+            userRepository.removeUserFromChannel(nickname, channel);
+        }
     }
 }
--- src/main/java/penisbot/state/UserRepository.java
+++ src/main/java/penisbot/state/UserRepository.java
@@ -1,22 +1,93 @@
 package penisbot.state;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 
 public class UserRepository {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     // Our collection of users
-    private ArrayList<User> users = new ArrayList<>();
+    // We could have various maps to nickname and channel to make lookups faster, but given
+    // the small size, just iterating through each time is probably not a big deal.
+    private final ArrayList<User> users = new ArrayList<>();
 
     public UserRepository() {
 
     }
 
     public synchronized void clearAll() {
+        logger.debug("Clearing all users from repository");
         users.clear();
     }
 
-    public synchronized void clearAllInChannel(String channelName) {
-        users.removeIf(user -> user.isInChannel(channelName));
+    public synchronized void removeChannel(String channelName) {
+        logger.debug("Removing " + channelName + " from all users in repository");
+
+        // Remove channel
+        for (User user : users) {
+            user.removeChannel(channelName);
+        }
+
+        // Prune any users no longer in any channels
+        users.removeIf(user -> user.getChannels().size() == 0);
+    }
+
+    public synchronized void removeUserFromChannel(String nickname, String channelName) {
+        logger.debug("Removing " + channelName + " from user " + nickname + " in repository");
+
+        // Grab user
+        User user = getUser(nickname);
+        if (user != null) {
+            // Remove channel from user
+            user.removeChannel(channelName);
+
+            // Remove user if no longer in any channels we know about
+            if (user.getChannels().size() == 0) {
+                users.remove(user);
+            }
+        }
+    }
+
+    public synchronized void removeUser(String nickname) {
+        logger.debug("Removing user " + nickname + " in repository");
+
+        users.removeIf(user -> user.getNickname().equalsIgnoreCase(nickname));
+    }
+
+    public synchronized User getUser(String nickname) {
+        for (User user : users) {
+            if (user.getNickname().equalsIgnoreCase(nickname)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public synchronized void upsertUser(String nickname, String channel) {
+        logger.debug("Upserting user " + nickname + " in channel " + channel + " in repository");
+
+        // Add user if doesn't exist
+        User user = getUser(nickname);
+        if (user == null) {
+            user = new User(nickname);
+            users.add(user);
+        }
+
+        // Add channel
+        user.addChannel(channel);
+    }
+
+    public synchronized void renameUser(String oldNickname, String newNickname) {
+        logger.debug("Renaming user " + oldNickname + " to " + newNickname + " in repository");
+
+        // Grab our user
+        User user = getUser(oldNickname);
+        if (user != null) {
+            user.setNickname(newNickname);
+        }
     }
 
 }
